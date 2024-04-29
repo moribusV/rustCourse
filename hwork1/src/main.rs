e29ff1610d5eb69adeae75ee6c3f339c3a7fc5b1
viktoriia <viktoriia.chvykova@exernal.thalesgroup.com>
2024-04-29 17:30:20 +0200

    hw1

--- hwork1/src/main.rs
+++ hwork1/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut name = String::new();
+    println!("Enter your name:");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line.");
+
+    println!("Hello dear {}!", name.trim());
 }
